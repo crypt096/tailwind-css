5bb93648d02cc2534643ea44dc0610b8e29831f3
crypt096 <acvjetan@gmail.com>
2021-01-11 17:37:01 +0100

    Add transitions.

--- public/index.js
+++ public/index.js
@@ -1,7 +1,6 @@
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
 
-console.log(burger, menu)
 burger.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
     menu.classList.remove('hidden')
